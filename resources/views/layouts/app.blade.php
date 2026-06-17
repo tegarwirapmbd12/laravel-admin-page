e951b3a428ee867192f954120db50c5c71ffbbc4
santik semarangkab <statistik.kominfokabsmg@gmail.com>
2026-06-18 06:25:36 +0700

    update composer.lock and fix blade template title syntax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', '{{ config('app.name') }}')</title>
+    <title>@yield('title', config('app.name'))</title>
 </head>
+
 <body>
     <div class="container mt-5">
         @yield('content')
     </div>
 </body>
+
 </html>
